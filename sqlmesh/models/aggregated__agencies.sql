c9c52ccf8dc476d20ed8b0bd9c02ecc96227982b
risa <lisaffang@gmail.com>
2025-01-25 23:03:06 -0600

    fix bad partition

--- sqlmesh/models/aggregated__agencies.sql
+++ sqlmesh/models/aggregated__agencies.sql
@@ -44,7 +44,7 @@ select
     , coalesce(resolution_last_3_months.median_days_to_close::text, 'unknown') as median_days_to_close
     , min(requests.created_date) as first_request_date
     , max(requests.created_date) as most_recent_request_date
-    , count(*) over (partition by requests.agency) as total_requests
+    , count(*) as total_requests
 
 from staging.service_requests as requests
 left join resolution_last_3_months on requests.agency = resolution_last_3_months.agency
